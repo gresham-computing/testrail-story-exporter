6c87353b2e8e415c19d25600518d96d0c299ccf3
James Laverack <jlaverack@greshamtech.com>
2017-12-17 03:01:33 +1100

    Add first-pass at script
    This uses some simple API calls and regular expressioins to parse
    information out of testrail.

---
+++ README.md
@@ -0,0 +1,36 @@
+# TestRail User Story Exporter
+
+Exports user stories from TestRail into a CSV. It expects a structure of directories named "Theme - $themeName" and
+"Epic - $epicName" to work correctly.
+
+Acceptance criteria, BDD, and other comments aren't handled. This assumes that any section that doesn't have any
+children is supposed to be a story, and sections that do have children are groupings of stories and not stories
+themselves.
+
+## Configuration
+
+You need a `testrail.ini` file in this directory that looks like this:
+
+```ini
+[testrail]
+Username = MyTestrailUsername
+ApiKey = MYTESTRAILAPIKEY
+Server = myserver.testrail.net
+SuiteId = 13
+```
+
+It's added to the `.gitignore` for security purposes (so you don't accidentally submit API credentials to a git repo).
+
+See the [TestRail Documentation](http://docs.gurock.com/testrail-api2/accessing#username_and_api_key) for information
+about generating API keys.
+
+## Usage
+
+Invoke as a python script in the usual way
+
+```bash
+python3 user-story-exporter.py
+```
+
+You'll get some output explaining any stories that couldn't be parsed. In this directory you'll get a `stories.csv` file
+as output.
---
+++ user-story-exporter.py
@@ -0,0 +1,101 @@
+import requests
+import configparser
+import re
+import csv
+
+
+def sections(testrail_config):
+    url = "https://{0}/index.php?/api/v2/get_sections/{1}".format(testrail_config['server'], testrail_config['suiteid'])
+    r = requests.get(url, auth=(testrail_config['username'], testrail_config['apikey']), headers={'Content-Type': 'application/json'})
+    return r.json()
+
+
+def match_line(prefix, story):
+    return re.search("^{0}(.*)$".format(prefix), story, re.MULTILINE).group(1).strip()
+
+
+def in_order_to(story):
+    return match_line("In order to", story)
+
+
+def i_want(story):
+    return match_line("I want", story)
+
+
+def as_a(story):
+    return match_line("As", story)
+
+
+def parse_user_story(node):
+    story = node['description']
+    try:
+        return {
+            'in order to': in_order_to(story),
+            'as': as_a(story),
+            'I want': i_want(story),
+        }
+    except (TypeError, AttributeError):
+        print("Unable to parse story {}".format(node['name']))
+        return None
+
+
+def parse_tree(sections):
+    for section in sections:
+        section['children'] = []
+
+    for section in sections:
+        if section['parent_id'] is not None:
+            # Find parent
+            parent = next(parent for parent in sections if parent['id'] == section['parent_id'])
+            parent['children'].append(section)
+
+    return [section for section in sections if section['parent_id'] is None]
+
+
+def give_story_context(story, context):
+    story['theme'] = context.get('theme')
+    story['epic'] = context.get('epic')
+
+
+def update_context(section_header, context):
+    context = context.copy()
+    if 'Epic' in section_header:
+        context['epic'] = re.search("Epic - (.*)", section_header).group(1).strip()
+    elif 'Theme' in section_header:
+        context['theme'] = re.search("Theme - (.*)", section_header).group(1).strip()
+    return context
+
+
+def flatten(list_of_lists):
+    return [item for sublist in list_of_lists for item in sublist]
+
+
+def walk_nodes(context, nodes):
+    return flatten(walk_tree(context, n) for n in nodes)
+
+
+def walk_tree(context, node):
+    if not node['children']:
+        # leaf node, this is a user story
+        story = parse_user_story(node)
+        if story is not None:
+            give_story_context(story, context)
+        return [story]
+    else:
+        # this is a grouping of stories
+        context = update_context(node['name'], context)
+        return walk_nodes(context, node['children'])
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('testrail.ini')
+    tree = parse_tree(sections(config['testrail']))
+    stories = [story for story in walk_nodes({}, tree) if story is not None]
+    keys = stories[0].keys()
+    filename = 'stories.csv'
+    with open(filename, 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(stories)
+    print("CSV file of {} stories written to {}".format(len(stories), filename))
