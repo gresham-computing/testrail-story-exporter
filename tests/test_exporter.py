1f569dc75518fdf047ea700001ca4c73ea72ec44
James Laverack <jlaverack@greshamtech.com>
2018-01-09 16:37:07 +1100

    Test story extraction

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from exporter import in_order_to, i_want, as_a, parse_user_story
+from exporter import in_order_to, i_want, as_a, parse_user_story, extract_stories
 
 
 class TestExporter(unittest.TestCase):
@@ -41,6 +41,60 @@ I want to create test data
             'name': 'My Story'
         }
         self.assertIsNone(parse_user_story(story))
+
+    def test_extract_stories(self):
+        sections = [
+            {
+                'id': 0,
+                'name': 'Theme - My Theme',
+                'parent_id': None
+            },
+            {
+                'id': 1,
+                'name': 'Epic - Epic One',
+                'parent_id': 0
+            },
+            {
+                'id': 2,
+                'name': 'My First Story',
+                'parent_id': 1,
+                'description': """
+In order to foo
+As a Software Engineer
+I want to bar"""
+            },
+            {
+                'id': 3,
+                'name': 'Epic - Epic Two',
+                'parent_id': 0
+            },
+            {
+                'id': 4,
+                'name': 'My Second Story',
+                'parent_id': 3,
+                'description': """
+In order to fizz
+As a Software Tester
+I want to buzz"""
+            },
+        ]
+
+        self.assertSequenceEqual(extract_stories(sections), [
+            {
+                'theme': 'My Theme',
+                'epic': 'Epic One',
+                'as': 'a Software Engineer',
+                'in order to': 'foo',
+                'I want': 'to bar'
+            },
+            {
+                'theme': 'My Theme',
+                'epic': 'Epic Two',
+                'as': 'a Software Tester',
+                'in order to': 'fizz',
+                'I want': 'to buzz'
+            }
+        ])
 
 
 if __name__ == '__main__':
