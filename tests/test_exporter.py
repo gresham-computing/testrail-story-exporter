7eda7da738ff1fbb683d43d3e5123641c3243016
James Laverack <jlaverack@greshamtech.com>
2018-01-09 15:56:57 +1100

    Flesh out tests for regex methods

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from exporter import in_order_to
+from exporter import in_order_to, i_want, as_a
 
 
 class TestExporter(unittest.TestCase):
@@ -18,6 +18,12 @@ I want to create test data
 
     def test_in_order_to(self):
         self.assertEqual(in_order_to(self.example_story), 'test this function')
+
+    def test_i_want(self):
+        self.assertEqual(i_want(self.example_story), 'to create test data')
+
+    def test_as_a(self):
+        self.assertEqual(as_a(self.example_story), 'a Software Engineer')
 
 
 if __name__ == '__main__':
